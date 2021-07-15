5f857ca5cc3d557c2a0f737afe5de888e2ee9fce
Mohamed Ashry <mohamedelashry7777@gmail.com>
2021-07-15 21:47:22 +0200

    Updating endpoints

--- solvers.py
+++ solvers.py
@@ -13,54 +13,57 @@ def solve(solver,goals_u):
     
     if solver == 'dp':
         goals = goals_u.split("#")
-        output = ''
+        output = []
         for index,goal in enumerate(goals) :
             path = os.path.join(dir_path,'dp_data.in')
             with open(path,'w') as f:
                 f.write(goal)
-            if index == len(goals)-1:
-                output += dp.solve(path)+'\n'
-            else:
-                output += dp.solve(path)+'#\n'
+            output.append(dp.solve(path))
+            # if index == len(goals)-1:
+            # else:
+                # output += dp.solve(path)+'#\n'
         return output
 
     elif solver == 'mip':
         goals = goals_u.split("#")
-        output = ''
+        output = []
         for index,goal in enumerate(goals) :
             path = os.path.join(dir_path,'mip_data.in')
             with open(path,'w') as f:
                 f.write(goal+'\n') 
-            if index == len(goals)-1:
-                output += mip.solve(path)+'\n'
-            else:
-                output += mip.solve(path)+'#\n'
+            output.append(mip.solve(path))
+            # if index == len(goals)-1:
+            #     output += mip.solve(path)+'\n'
+            # else:
+            #     output += mip.solve(path)+'#\n'
         return output
 
     elif solver == 'baseline':
         goals = goals_u.split("#")
-        output = ''
+        output = []
         for index,goal in enumerate(goals) :
             path = os.path.join(dir_path,'baseline_data.in')
             with open(path,'w') as f:
                 f.write(goal+'\n') 
-            if index == len(goals)-1:
-                output += baseline.solve(path)+'\n'
-            else:
-                output += baseline.solve(path)+'#\n'
+            output.append(baseline.solve(path))
+            # if index == len(goals)-1:
+            #     output += baseline.solve(path)+'\n'
+            # else:
+            #     output += baseline.solve(path)+'#\n'
         return output
 
     elif solver=='meta':
         goals = goals_u.split("#")
-        output = ''
+        output = []
         for index,goal in enumerate(goals) :
             path = os.path.join(dir_path,'heuristics_data.in')
             with open(path,'w') as f:
                 f.write(goal+'\n') 
-            if index == len(goals)-1:
-                output += heuristics.solve(path)+'\n'
-            else:
-                output += heuristics.solve(path)+'#\n'
+            output.append(heuristics.solve(path))
+            # if index == len(goals)-1:
+            #     output += heuristics.solve(path)+'\n'
+            # else:
+            #     output += heuristics.solve(path)+'#\n'
         return output 
     
         
