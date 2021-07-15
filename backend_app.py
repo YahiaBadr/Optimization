66c457ed7284a3ada235338ba71ec2fa57492b32
Mohamed Ashry <mohamedelashry7777@gmail.com>
2021-07-15 14:20:20 +0200

    Adding cors

--- backend_app.py
+++ backend_app.py
@@ -3,6 +3,7 @@
 
 
 from flask import Flask, request 
+from flask_cors import CORS, cross_origin
 
 import solvers
 
@@ -11,13 +12,18 @@ import solvers
 
 
 app = Flask (__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 
 @app.route('/')
+@cross_origin()
 def index():
    return "Black Opt Backend Server"
 
 
 @app.route('/baseline',methods=['POST','GET'])
+@cross_origin()
 def solve_baseline():
     if request.method =='GET':
         return "This the Baseline Solution"
@@ -25,6 +31,7 @@ def solve_baseline():
     return solvers.solve('baseline',req['data'])
 
 @app.route('/mip',methods=['POST','GET'])
+@cross_origin()
 def solve_mip():
     if request.method =='GET':
         return "This the MIP Solution"
@@ -35,6 +42,7 @@ def solve_mip():
 
 
 @app.route('/dp',methods=['POST','GET'])
+@cross_origin()
 def solve_dp():
     if request.method =='GET':
         return "This the DP Solution"
@@ -44,6 +52,7 @@ def solve_dp():
 
 
 @app.route('/meta',methods=['POST','GET'])
+@cross_origin()
 def solve_meta():
     if request.method =='GET':
         return "This the Meta Solution"
