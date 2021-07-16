558894844e1849c5411960d8107560579de73023
Mohamed Ashry <mohamedelashry7777@gmail.com>
2021-07-17 01:22:04 +0200

    Adding map feature

--- solvers.py
+++ solvers.py
@@ -5,65 +5,72 @@
 import os 
 import dp ,baseline,mip,heuristics
 
-
+import numpy as np
 
 def solve(solver,goals_u): 
-    dir_path = 'web_input'
+    dir_path = 'web_IO'
     os.makedirs(dir_path,exist_ok=True)
-    
+    goals = goals_u.split("#")
+    output = []
     if solver == 'dp':
-        goals = goals_u.split("#")
-        output = []
-        for index,goal in enumerate(goals) :
+        
+        for goal in goals :
             path = os.path.join(dir_path,'dp_data.in')
             with open(path,'w') as f:
                 f.write(goal)
             output.append(dp.solve(path))
-            # if index == len(goals)-1:
-            # else:
-                # output += dp.solve(path)+'#\n'
-        return output
+            
+                
 
     elif solver == 'mip':
-        goals = goals_u.split("#")
-        output = []
-        for index,goal in enumerate(goals) :
+      
+        for goal in goals :
             path = os.path.join(dir_path,'mip_data.in')
             with open(path,'w') as f:
                 f.write(goal+'\n') 
             output.append(mip.solve(path))
-            # if index == len(goals)-1:
-            #     output += mip.solve(path)+'\n'
-            # else:
-            #     output += mip.solve(path)+'#\n'
-        return output
 
     elif solver == 'baseline':
-        goals = goals_u.split("#")
-        output = []
-        for index,goal in enumerate(goals) :
+        
+        for goal in goals :
             path = os.path.join(dir_path,'baseline_data.in')
             with open(path,'w') as f:
                 f.write(goal+'\n') 
             output.append(baseline.solve(path))
-            # if index == len(goals)-1:
-            #     output += baseline.solve(path)+'\n'
-            # else:
-            #     output += baseline.solve(path)+'#\n'
-        return output
+            
 
     elif solver=='meta':
-        goals = goals_u.split("#")
-        output = []
-        for index,goal in enumerate(goals) :
+       
+        for goal in goals :
             path = os.path.join(dir_path,'heuristics_data.in')
             with open(path,'w') as f:
                 f.write(goal+'\n') 
             output.append(heuristics.solve(path))
-            # if index == len(goals)-1:
-            #     output += heuristics.solve(path)+'\n'
-            # else:
-            #     output += heuristics.solve(path)+'#\n'
-        return output 
+    if len(goals)==1:
+
+
+    
+    return output
     
         
+def get_reqs_branches_positions(lines):
+    arr = np.array(lines[0].split(' '), dtype='int64')
+    r = 1+arr[2]
+    reqs = []
+    i = arr[2]+arr[1]
+    branches = []
+    while r <1+arr[2]+arr[1]:
+
+        potential = np.array(arr[r].split(' '), dtype='int64')
+        if len(potential)==4:
+            reqs.append((potential[0],potential[1]))
+        r+=1
+    while i < len(arr):
+        potential = np.array(arr[i].split(' '), dtype='int64')
+        if len(potential)==4:
+            branches.append((potential[0],potential[1]))
+        i+=1
+    
+
+
+
