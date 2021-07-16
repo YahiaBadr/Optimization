e9bfa694fb46b9728adc53879d00fa08329741e0
MennaFadali <menafadali98@gmail.com>
2021-07-16 23:31:52 +0200

    Fixing the primal tolerance bug in the mip solver.

--- mip.py
+++ mip.py
@@ -6,81 +6,67 @@ from ortools.linear_solver import pywraplp
 
 alpha = 1
 beta = 0
-gamma = 0
+theta = 0
 
 
 def MIPSolution(path):
     b, s, r, m, cap, rs, slots, dist, p, serves, d = open_test(path)
-    solver = pywraplp.Solver.CreateSolver('SCIP')
-    x = []
-    M = 1
     s = np.array(s, dtype='int64')
     cap = np.array(cap, dtype='int64')
     rs = np.array(rs, dtype='int64')
     slots = np.array(slots, dtype='int64')
 
-    ### creating varirables and adding constraint 1 to the domain fo the variables ###
-    for i in range(r):
-        x.append([])
-        for j in range(b):
-            x[i].append([])
-            for k in range(s[j]):
-                x[i][j].append([])
-                for w in range(cap[j]):
-                    domain_constraint = 1
+    # Lowering default primal tolerance
+    # refer to this for more details: https://github.com/google/or-tools/discussions/2295
+    solver_parameters = pywraplp.MPSolverParameters()
+    solver_parameters.SetDoubleParam(
+        pywraplp.MPSolverParameters.PRIMAL_TOLERANCE, 0.001)
+    solver = pywraplp.Solver.CreateSolver('SCIP')
+    objective = solver.Objective()
+    vars = []
 
-                    # constraint 1
-                    if not (serves[j][w][rs[i]] == 1
-                            and ((slots[rs[i]][j] + k) <= s[j])
-                            and dist[i][j] <= d):
-                        domain_constraint = 0
-                    ### end of constraint 1 ###
-                    x[i][j][k].append(solver.IntVar(
-                        0, domain_constraint, 'x%i%i%i%i' % (i, j, k, w)))
-    ## constraint 2 ###
     for i in range(r):
-        summation = 0
+        vars.append([])
+        sum = 0
         for j in range(b):
+            vars[i].append([])
             for k in range(s[j]):
+                vars[i][j].append([])
                 for w in range(cap[j]):
-                    summation += x[i][j][k][w]
-        solver.Add(summation <= 1)
-    ### end of constraint 2 ###
+                    x = None
+                    if serves[j][w][rs[i]] and dist[i][j] <= d and (slots[rs[i]][j] + k) <= s[j]:
+                        x = solver.IntVar(0, 1, 'x%i%i%i%i' % (i, j, k, w))
+                        sum += x
+                        objective.SetCoefficient(
+                            x, (alpha-beta*dist[i][j]+p[i]*theta))
+                    vars[i][j][k].append(x)
+        solver.Add(sum <= 1)
 
-    ### constraint 3 ###
     for i in range(r):
         for j in range(b):
             for k in range(s[j]):
                 for w in range(cap[j]):
-                    for i2 in range(r):
-                        if i2 == i or k + slots[rs[i]][j] > s[j]:
-                            continue
-                        for k2 in range(k, k + slots[rs[i]][j]):
-                            solver.Add(x[i][j][k][w] <= (1-x[i2][j][k2][w]))
-    # end of constraint 3
-    ### objective function ###
-    F0 = 0
-    F1 = 0
-    F2 = 0
-    for i in range(r):
-        for j in range(b):
-            for k in range(s[j]):
-                for w in range(cap[j]):
-                    F0 = F0 + x[i][j][k][w]
-                    F1 = F1 + x[i][j][k][w] * dist[i][j]
-                    F2 = F2 + x[i][j][k][w] * p[i]
+                    if vars[i][j][k][w] is None:
+                        continue
+                    for k2 in range(k, k+slots[rs[i]][j]):
+                        for i2 in range(r):
+                            if i == i2 or vars[i2][j][k2][w] is None:
+                                continue
+                            solver.Add(vars[i][j][k][w] <=
+                                       (1-vars[i2][j][k2][w]))
 
-    objective_eqn = (alpha*F0) + (beta*F1) - (gamma*F2)
-    solver.Maximize(objective_eqn)
-    solver.SetTimeLimit(5000)
-    solver.Solve()
-    score = solver.Objective().Value()
+    objective.SetMaximization()
+    solver.SetTimeLimit(10000)
+    status = solver.Solve(solver_parameters)
+    score = objective.Value()
     solution = []
     for i in range(r):
         for j in range(b):
             for k in range(s[j]):
                 for w in range(cap[j]):
-                    if x[i][j][k][w].solution_value() == 1:
+                    if vars[i][j][k][w] is None:
+                        continue
+                    if vars[i][j][k][w].solution_value() == 1:
                         solution.append((i+1, j+1, k+1, w+1))
 
     return score, solution
